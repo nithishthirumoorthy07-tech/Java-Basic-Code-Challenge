d355fc06174fd45244e3544e68b78369b4a1c9cc
Nithish <nithishthirumoorthy07@gmail.com>
2026-07-10 22:12:42 +0530

    Create DivideTwoVariables.java

---
+++ DivideTwoVariables.java
@@ -0,0 +1,10 @@
+public class DivideTwoVariables {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 6;
+
+        int result = x / y;
+
+        System.out.println(result);
+    }
+}
